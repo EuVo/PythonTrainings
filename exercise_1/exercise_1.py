db4cc8141580246468d1e1fc40ba34ad8fb4a2d2
zhekazhvolgin <evolgin@ptsecurity.com>
2022-01-18 14:09:22 +0300

    finished exercise_1

--- exercise_1/exercise_1.py
+++ exercise_1/exercise_1.py
@@ -25,6 +25,44 @@ def task3():
     percents = 10000 * (1+0.1*3)
     print(f'Money with percents: {money + percents}')
 
+#// and % execution
+def task4():
+    #students count
+    n = 4
+
+    #apples count in bag
+    k = 6
+
+    print(f'Apples to each students: {k//n}')
+    print(f'Apples remain in bag: {k%n}')
+
+#Print clock value after adding N minutes to day beggining
+def task5():
+    #minutes past from day beginning
+    n = 1439
+
+    #convert minutes to hh and mm
+    hours = n // 60
+    minutes = n % 60
+
+    if(hours < 10 and minutes < 10):
+        hours = "0" + str(hours)
+        minutes = "0" + str(minutes)
+    elif(hours > 10 and minutes < 10 ):
+        minutes = "0" + str(minutes)
+    elif (hours < 10 and minutes > 10):
+        hours = "0" + str(hours)
+    else:
+        pass
+
+    if(hours < 24):
+        print(f'Clock value: {hours}:{minutes}')
+    else:
+        print("The 'hours' parameter can not be more or equal to 24")
+
+
+
+
 
 
 
